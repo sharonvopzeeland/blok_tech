f29cb4a9a825e6564748706ff2a454664700dc10
sharon <svanopzeeland@kpnmail.nl>
2022-02-27 16:18:54 +0100

    update 2.0 video
    we gaan weer even updaten

--- matchingapp/server.js
+++ matchingapp/server.js
@@ -2,9 +2,28 @@ const express = require('express')
 const app = express();
 const port = 1111;
 
+
+/* ROUTES */
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/index', (req, res) => {
+    res.send('Dit is de index!')
+  })
+
+app.get('/home', (req, res) => {
+    res.send('Dit is home!')
+})
+
+app.get('/profielaanmaken', (req, res) => {
+    res.send('Dit is een profiel aanmaken!')
+})
+
+app.get('/chats', (req, res) => {
+    res.send('Dit zijn chats!')
+})
+
 
 app.listen(port, () => {
   console.log(`web server running on http://localhost:${port}`)
