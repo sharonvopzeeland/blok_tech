4464a22058374281fadf70ffaea897a14091804f
sharon <svanopzeeland@kpnmail.nl>
2022-03-04 13:39:37 +0100

    beginnend aan meer ejs - profielen aanmaken
    here we goooooo

--- matchingapp/server.js
+++ matchingapp/server.js
@@ -3,6 +3,30 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+const profielen = [{
+  naam: "Jopie",
+  email: "jopie123@gmail.com",
+  leeftijd: 81,
+  hobby: "Puzzelen",
+  omgeving: "Noord-Holland"
+}, 
+{
+  naam: "Gerard",
+  email: "gerardus@gmail.com",
+  leeftijd: 80,
+  hobby: "Tuinieren",
+  omgeving: "Zuid-Holland"
+},
+{
+  naam: "Mary",
+  email: "maryvo@gmail.com",
+  leeftijd: 76,
+  hobby: "Breien",
+  omgeving: "Friesland"
+}
+
+]
+
 
 // static files (middleware)
 app.use(express.static('static'))
@@ -24,6 +48,17 @@ app.get('/profiel', (req, res) => {
   res.render('profielaanmaken')
 })
 
+app.post('/matches', (req, res) => {
+    //hobby bepalen
+    //gegevens bewaren
+    //lijst met matches op basis van hobby
+  
+
+  res.render('matches', {profielen})
+})
+
+
+// van tutorial
 //app.get('', (req, res) => {
 //  res.render('tutorial', { text: 'this is ejs'})
 // })
