4b630851e673ce2bb83f757a398e36a8917661c3
sharon <svanopzeeland@kpnmail.nl>
2022-03-09 14:35:12 +0100

    gulp = added :)
    bundler (build toos), wat handig!!!

---
+++ matchingapp/gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const nodemon = require('gulp-nodemon');
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('css', () => (
+    gulp.src('./static/styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('static/dist'))
+    ))
+
+
+gulp.task('watch', () => (
+    gulp.watch('./static/styles/*.css', gulp.series('css'))
+))
+
+gulp.task('start', (done) => (
+    nodemon({
+        script: 'server.js',
+        ext: 'css',
+        tasks: ['css'],
+        ignore: ['static/dist'],
+        done: done
+    })
+))
+
+gulp.task('default', gulp.series('css', 'start'))
+
+
