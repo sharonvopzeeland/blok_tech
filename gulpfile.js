2c43c291dd732c4d5c7fcc2bbd570378d50620b1
sharon <svanopzeeland@kpnmail.nl>
2022-03-14 13:38:41 +0100

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
-const nodemon = require('gulp-nodemon') || null;
 const cleanCSS = require('gulp-clean-css');
+
+if(process.env.NODE_ENV !== 'production') {
+    const nodemon = require('gulp-nodemon');
+}
 
 gulp.task('css', () => (
     gulp.src('./static/styles/*.css')
@@ -15,15 +18,17 @@ gulp.task('watch', () => (
     gulp.watch('./static/styles/*.css', gulp.series('css'))
 ))
 
-gulp.task('start', (done) => (
-    nodemon({
-        script: 'server.js',
-        ext: 'css',
-        tasks: ['css'],
-        ignore: ['static/dist'],
-        done: done
-    })
-))
+if(process.env.NODE_ENV !== 'production') {
+    gulp.task('start', (done) => (
+        nodemon({
+            script: 'server.js',
+            ext: 'css',
+            tasks: ['css'],
+            ignore: ['static/dist'],
+            done: done
+        })
+    ))
+}
 
 gulp.task('default', gulp.series('css', 'start'))
 
