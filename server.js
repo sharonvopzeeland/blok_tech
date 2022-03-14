98cbfd10fdf2e174f0d4da29ac81b5893083239d
sharon <svanopzeeland@kpnmail.nl>
2022-03-14 13:47:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config()
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 const slug = require('slug');
