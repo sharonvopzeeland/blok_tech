80b158fd724ef1284ae02f95bc2cd9a448d6db82
sharon <svanopzeeland@kpnmail.nl>
2022-03-15 16:39:39 +0100

    hamburger menu added

---
+++ static/scripts/script.js
@@ -0,0 +1,8 @@
+const hamburgerMenu = document.querySelector("nav button");
+
+hamburgerMenu.addEventListener("click", toggleHamburger);
+
+function toggleHamburger() {
+  const navEl = document.querySelector("nav");
+  navEl.classList.toggle("toonHamburger");
+}
